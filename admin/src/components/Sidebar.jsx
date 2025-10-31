840887b0203d3cc5da2034d9d882ca70ebe0743f
priyansudash1999 <1999priyansudash@gmail.com>
2025-10-31 20:43:36 +0530

    List the Item in admin page and can remove the food item by admin also and change the styling of Sidebar

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -4,20 +4,42 @@ import { NavLink } from 'react-router-dom'
 
 const Sidebar = () => {
   return (
-    <div className='w-[18%] min-h-screen border-[1.5px] border-[#9a9a9a]'>
+    <div className="w-[18%] min-h-screen border border-[#9a9a9a]">
       <div className="pt-10 pl-[20%] flex flex-col gap-4">
-        <NavLink to="/add" className="flex items-center gap-6 border border-[#d3d3d3] border-r-0 py-2 px-2 rounded-tl-[3px] rounded-bl-[3px] rounded-tr-none rounded-br-none cursor-pointer active:bg-[#fff0ed] active:border-orange-500">
-          <img className='max-[900px]:hidden' src={assets.add_icon} alt="" />
-          <p>Add Item</p>
+
+        <NavLink
+          to="/add"
+          className="flex items-center gap-6 border border-[#d3d3d3] border-r-0 py-2 px-2 
+          rounded-tl-[3px] rounded-bl-[3px] cursor-pointer
+          active:bg-[#fff0ed] active:border-orange-500
+          max-[1800px]:justify-center"   // 👈 center icon below 1800px
+        >
+          <img src={assets.add_icon} alt="" />
+          <p className="min-[1800px]:block max-[1800px]:hidden">Add Item</p>
         </NavLink>
-        <NavLink to="/list" className="flex items-center gap-6 border border-[#d3d3d3] border-r-0 py-2 px-2 rounded-tl-[3px] rounded-bl-[3px] rounded-tr-none rounded-br-none cursor-pointer active:bg-[#fff0ed] active:border-orange-500">
-          <img className='max-[900px]:hidden' src={assets.order_icon} alt="" />
-          <p >List Items</p>
+
+        <NavLink
+          to="/list"
+          className="flex items-center gap-6 border border-[#d3d3d3] border-r-0 py-2 px-2 
+          rounded-tl-[3px] rounded-bl-[3px] cursor-pointer
+          active:bg-[#fff0ed] active:border-orange-500
+          max-[1800px]:justify-center"
+        >
+          <img src={assets.order_icon} alt="" />
+          <p className="min-[1800px]:block max-[1800px]:hidden">List Items</p>
         </NavLink>
-        <NavLink to="/order" className="flex items-center gap-6 border border-[#d3d3d3] border-r-0 py-2 px-2 rounded-tl-[3px] rounded-bl-[3px] rounded-tr-none rounded-br-none cursor-pointer active:bg-[#fff0ed] active:border-orange-500">
-          <img className='max-[900px]:hidden' src={assets.order_icon} alt="" />
-          <p>Orders</p>
+
+        <NavLink
+          to="/order"
+          className="flex items-center gap-6 border border-[#d3d3d3] border-r-0 py-2 px-2 
+          rounded-tl-[3px] rounded-bl-[3px] cursor-pointer
+          active:bg-[#fff0ed] active:border-orange-500
+          max-[1800px]:justify-center"
+        >
+          <img src={assets.order_icon} alt="" />
+          <p className="min-[1800px]:block max-[1800px]:hidden">Orders</p>
         </NavLink>
+
       </div>
     </div>
   )
--- admin/src/pages/DisplayList.jsx
+++ admin/src/pages/DisplayList.jsx
@@ -1,8 +1,65 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { toast } from 'react-toastify'
 
 const DisplayList = () => {
+
+  const [data, setData] = useState([])
+  const url = "http://localhost:4000"
+  const fetchList = async () => {
+    const res = await axios.get(`${url}/api/food/list`)
+    // console.log(res.data)
+    if(res.data.success){
+      setData(res.data.data)
+    }
+    else{
+      toast.error(res.data.message)
+    }
+  }
+
+  const removeFoorItem = async(food_id) => {
+    // console.log(food_id);
+    const res = await axios.post(`${url}/api/food/remove`,{id: food_id})
+    await fetchList()
+    if(res.data.success){
+      toast.success(res.data.message)
+    }
+    else{
+      toast.error("Error")
+    }
+  }
+
+  useEffect(() => {
+    fetchList()
+  }, [])
+
   return (
-    <div>DisplayList</div>
+    <div className='list add flex flex-col gap-2'>
+      <p className='text-lg font-semibold text-center'>All Foods List</p>
+      <div className="list-table mt-1">
+        <div className="grid grid-cols-[1.25fr_2fr_1fr_1fr_0.5fr] md:grid-cols-[1fr_2fr_1fr_1fr_0.5fr] items-center gap-2 px-3 py-2 border border-[#cacaca] text-md rounded bg-[#f0f0f0] sm:w-[90vw]">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Action</b>
+        </div>
+        {
+          data.map((product, ind) => {
+            return(
+              <div key={ind} className='grid grid-cols-[1fr_2fr_1fr_1fr_0.5fr] md:grid-cols-[1fr_2fr_1fr_1fr_0.5fr] items-center gap-2 px-3 py-2 border border-[#cacaca] rounded '>
+                <img src={`${url}/images/`+product.image} alt="" className='w-[50px]'/>
+                <p className='p-2 ml-2 md:p-0 md:ml-0'>{product.name}</p>
+                <p>{product.category}</p>
+                <p>${product.price}</p>
+                <p onClick={() => removeFoorItem(product._id)} className='text-red-600 cursor-pointer'>X</p>
+              </div>
+            )
+          })
+        }
+      </div>
+    </div>
+
   )
 }
 
