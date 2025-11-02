c789a3b1183e8a28fbf57a4f23f0445ee6e4d815
priyansudash1999 <1999priyansudash@gmail.com>
2025-11-02 21:48:47 +0530

    User able to login and register

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,7 +6,35 @@ import validator from "validator"
 
 // Login User
 const loginUser = async(req, res) => {
-  
+  const{email, password} = req.body
+  try {
+    const user = await userModel.findOne({email})
+    if(!user){
+      return res.json({
+        success: false,
+        message: "User does not exist"
+      })
+    }
+    const isMatch = await bcrypt.compare(password, user.password)
+    if(!isMatch){
+      return res.json({
+        success: false,
+        message: "Invalid Password"
+      })
+    }
+
+    const token = createToken(user._id)
+    res.json({
+      success: true,
+      token
+    })
+  } catch (error) {
+    console.error(error);
+    res.json({
+      success: false,
+      message: "Error"
+    })
+  }
 }
 
 const createToken = (id) => {
